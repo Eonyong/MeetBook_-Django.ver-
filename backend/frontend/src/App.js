0a7e518ff5007ed2439c40f63116317140679f43
Eonyong <unjoo94@naver.com>
2022-04-20 23:34:49 +0900

    navbar 만들기

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Button, Link, Toolbar, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position='fixed'color='secondary'>
+        <Toolbar>
+          <Button><Typography color='white'>로그인</Typography></Button>
+          <Button >회원가입</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
